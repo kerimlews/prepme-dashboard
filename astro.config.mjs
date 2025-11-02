1bea04c67780cd1c5c8b7907c026393a9a75d953
kerimlews <kerimalihodza22@gmail.com>
2025-11-02 10:03:14 +0100

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
   integrations: [react()],
 });
